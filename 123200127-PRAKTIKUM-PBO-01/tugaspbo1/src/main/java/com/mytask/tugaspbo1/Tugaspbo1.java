c1d5b57c59263fe0b8a35ba0f794703c0c8b742a
admin-farhan <muhfarhan912391@gmail.com>
2023-03-17 18:14:49 +0700

    Tugas 1 Praktikum PBO IF-G

---
+++ 123200127-PRAKTIKUM-PBO-01/tugaspbo1/src/main/java/com/mytask/tugaspbo1/Tugaspbo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mytask.tugaspbo1;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Tugaspbo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
